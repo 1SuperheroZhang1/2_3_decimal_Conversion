da6613fc79fe04363180a70173e53905a3804846
zhangcy <2304417358@qq.com>
2023-01-02 20:17:16 +0800

    C语言督学营 -  2.3 进制转换

---
+++ cmake-build-debug/CMakeFiles/2_3_decimal_Conversion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_3_decimal_Conversion.exe"
+  "2_3_decimal_Conversion.exe.manifest"
+  "2_3_decimal_Conversion.pdb"
+  "CMakeFiles/2_3_decimal_Conversion.dir/main.c.obj"
+  "lib2_3_decimal_Conversion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2_3_decimal_Conversion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
